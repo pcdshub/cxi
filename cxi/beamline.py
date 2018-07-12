dd3477cf11b08654d4f10333b107218f5a3735e2
JJ <jj@dhcp-visitor-222-29.slac.stanford.edu>
2018-07-12 14:05:06 -0700

    WIP

--- cxi/beamline.py
+++ cxi/beamline.py
@@ -1,1 +1,33 @@
 from hutch_python.utils import safe_load
+from devices import Injector
+
+# from cxi.db import cxi
+from cxi.db import cxi_pulsepicker
+from cxi.db import daq
+
+try:
+    def daq_repeat(seconds=10):
+        while True:
+            daq.begin(duration=seconds, wait=True)
+            daq.end_run()
+
+except KeyboardInterrupt:
+    daq.end_run()
+
+def daq_fixed_runs(runtime, darktime = 0):
+#   print "Run time =" run_time
+#   print "Dark time =" dark_time
+    while True:
+        if darktime > 0:
+            cxi.pulsepicker.open(wait=True)
+            daq.record(duration=run_time, wait=True)
+            daq.end_run()
+
+        else:
+            cxi.pulsepicker.close(wait=True)
+            daq.record(duration=darktime, wait=True)
+            cxi.pulsepicker.open(wait=True)
+            daq.record(duration=runtime,wait=True)
+            daq.end_run()
+
+
---
+++ cxi/devices.py
@@ -0,0 +1,25 @@
+from ophyd import (Device, EpicsSignal, FormattedComponent as FC)
+
+class Injector(Device):
+
+    coarseX = FC(EpicsSignal, '{self._coarseX_name}')
+    coarseY = FC(EpicsSignal, '{self._coarseY_name}')
+    coarseZ = FC(EpicsSignal, '{self._coarseZ_name}')
+                
+    fineX = FC(EpicsSignal, '{self._fineX_name}')
+    fineY = FC(EpicsSignal, '{self._fineY_name}')
+    fineZ = FC(EpicsSignal, '{self._fineZ_name}')
+                            
+    def __init__(self, coarseX_name, coarseY_name,
+                       coarseZ_name, fineX_name, fineY_name,
+                       fineZ_name, **kwargs):
+                                        
+        self._coarseX_name = coarseX_name
+        self._coarseY_name = coarseY_name
+        self._coarseZ_name = coarseZ_name
+                                                             
+        self._fineX_name = fineX_name
+        self._fineY_name = fineY_name
+        self._fineZ_name = fineZ_name
+
+        super().__init__(**kwargs)
