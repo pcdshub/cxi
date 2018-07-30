1c3d01be92f4930b6dcbbfdfc47d5074aa68acda
jeannas <jeannas@users.noreply.github.com>
2018-07-30 14:05:37 -0700

    DEV: create injector objects

--- cxi/beamline.py
+++ cxi/beamline.py
@@ -29,34 +29,33 @@ def daq_fixed_runs(runtime, darktime = 0):
             cxi.pulsepicker.open(wait=True)
             daq.record(duration=runtime,wait=True)
             daq.end_run()
-            
-with safe_load('PI1'):
-    PI1 = {'injector_name': 'PI1 Injector',
-           'coarseX_name': 'CXI:PI1:MMS:01',
-           'coarseY_name': 'CXI:PI1:MMS:02',
-           'coarseZ_name': 'CXI:PI1:MMS:03',
-           'fineX_name': 'CXI:USR:MMS:01',
-           'fineY_name': 'CXI:USR:MMS:02',
-           'fineZ_name': 'CXI:USR:MMS:03'}
-    injector_PI1 = Injector(**PI1)
-    
-with safe_load('PI2'):
-    PI2 = {'injector_name': 'PI2 Injector',
-           'coarseX_name': 'CXI:PI2:MMS:01',
-           'coarseY_name': 'CXI:PI2:MMS:02',
-           'coarseZ_name': 'CXI:PI2:MMS:03',
-           'fineX_name': 'CXI:PI2:MMS:04',
-           'fineY_name': 'CXI:PI2:MMS:05',
-           'fineZ_name': 'CXI:PI2:MMS:06'}
-    injector_PI2 = Injector(**PI2)
-    
-with safe_load('PI3'):
-    PI3 = {'injector_name': 'PI3 Injector',
-           'coarseX_name': 'CXI:PI3:MMS:01',
-           'coarseY_name': 'CXI:PI3:MMS:02',
-           'coarseZ_name': 'CXI:PI3:MMS:03',
-           'fineX_name': 'CXI:PI3:MMS:04',
-           'fineY_name': 'CXI:PI3:MMS:05',
-           'fineZ_name': 'CXI:PI3:MMS:06'}
-    injector_PI3 = Injector(**PI3)    
 
+with safe_load('PI1'):
+    PI1 = {'name': 'PI1_injector',
+           'coarseX': 'CXI:PI1:MMS:01',
+           'coarseY': 'CXI:PI1:MMS:02',
+           'coarseZ': 'CXI:PI1:MMS:03',
+           'fineX': 'CXI:USR:MMS:01',
+           'fineY': 'CXI:USR:MMS:02',
+           'fineZ': 'CXI:USR:MMS:03'}
+    injector_PI1 = Injector(**PI1)
+
+with safe_load('PI2'):
+    PI2 = {'name': 'PI2_injector',
+           'coarseX': 'CXI:PI2:MMS:01',
+           'coarseY': 'CXI:PI2:MMS:02',
+           'coarseZ': 'CXI:PI2:MMS:03',
+           'fineX': 'CXI:PI2:MMS:04',
+           'fineY': 'CXI:PI2:MMS:05',
+           'fineZ': 'CXI:PI2:MMS:06'}
+    injector_PI2 = Injector(**PI2)
+
+with safe_load('PI3'):
+    PI3 = {'name': 'PI3_injector',
+           'coarseX': 'CXI:PI3:MMS:01',
+           'coarseY': 'CXI:PI3:MMS:02',
+           'coarseZ': 'CXI:PI3:MMS:03',
+           'fineX': 'CXI:PI3:MMS:04',
+           'fineY': 'CXI:PI3:MMS:05',
+           'fineZ': 'CXI:PI3:MMS:06'}
+    injector_PI3 = Injector(**PI3)
